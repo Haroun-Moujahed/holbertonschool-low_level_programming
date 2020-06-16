c7e55424b7868c16980042dc574fe39c675a031f
Haroun-Moujahed <haroun.moujahed.tu@gmail.com>
2020-06-16 17:56:40 +0000

    11 fixed betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,13 +18,13 @@ else if (n > 98)
 for (i = n; i >= 98; i--)
 if (i != 98)
 printf("%d, ", i);
-else 
-printf("%d",i);
+else
+printf("%d", i);
 else
 for (i = n; i <= 98; i++)
 if (i != 98)
 printf("%d, ", i);
 else
-printf("%d",i);
+printf("%d", i);
 printf("\n");
 }
