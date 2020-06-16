eec8e040a478ad9e2c894bdbdb6dcb79e00e8a50
Haroun-Moujahed <haroun.moujahed.tu@gmail.com>
2020-06-16 11:38:32 +0000

    2 and 8 fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,36 +1,37 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - printf alphabet 10 times
+ * jack_bauer - displays the the 24 hours of the day
  *
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
-	int h1,h2,m,s;
+	int h1, h2, m, s;
+
 	for (h1 = 0; h1 <= 2; h1++)
-		if(h1 == 2)
-			for(h2 = 0; h2 <= 3; h2++)
-				for(m = 0; m <= 5; m++)
-					for(s = 0 ; s <= 9 ; s++)
+		if (h1 == 2)
+			for (h2 = 0; h2 <= 3; h2++)
+				for (m = 0; m <= 5; m++)
+					for (s = 0; s <= 9; s++)
 					{
-						_putchar(h1 + '0');
-						_putchar(h2 + '0');
-						_putchar(':');
-						_putchar(m + '0');
-						_putchar(s + '0');
-						_putchar('\n');
+						_putchar (h1 + '0');
+						_putchar (h2 + '0');
+						_putchar (':');
+						_putchar (m + '0');
+						_putchar (s + '0');
+						_putchar ('\n');
 					}
 		else
-			for(h2 = 0; h2 <= 9; h2++)
-				for(m = 0; m <= 5; m++)
-					for(s = 0 ; s <= 9 ; s++)
+			for (h2 = 0; h2 <= 9; h2++)
+				for (m = 0; m <= 5; m++)
+					for (s = 0; s <= 9; s++)
 					{
-						_putchar(h1 + '0');
-						_putchar(h2 + '0');
-						_putchar(':');
-						_putchar(m + '0');
-						_putchar(s + '0');
-						_putchar('\n');
+						_putchar (h1 + '0');
+						_putchar (h2 + '0');
+						_putchar (':');
+						_putchar (m + '0');
+						_putchar (s + '0');
+						_putchar ('\n');
 					}
 }
